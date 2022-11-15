1385c90f84549010a98141075a111e5a274f3e2c
KadiyalaSaiSowmith <sai.kadiyala@netcracker.com>
2022-11-15 15:13:46 +0530

    minor update in task 6 (including iterator)

--- src/Task6.java
+++ src/Task6.java
@@ -8,22 +8,29 @@ public class Task6 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[10];
+        ArrayList<Integer> arr = new ArrayList<Integer>();
         System.out.println("Enter the elements into the array");
-        for(int i=0;i<arr.length;i++)
+        for(int i=0;i<10;i++)
         {
-            arr[i]=sc.nextInt();
+            int x =sc.nextInt();
+            arr.add(x);
         }
+        // Conversion of array to ArrayList
+        // using Arrays.asList
+       // List all = Arrays.asList(arr);
+        Iterator<Integer> it = arr.iterator();
         System.out.println();
-        System.out.println("The required output array which iterates only over the Even Numbers");
+        System.out.println("The required output array which iterates only over the Even Numbers USING ITERATOR");
 
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]%2==0)
-            {
-                System.out.print(arr[i]+" ");
-            }
-        }
+       while(it.hasNext())
+       {
+           int c=it.next();
+           if(c%2!=0)
+           {
+               it.remove();
+           }
+       }
+       System.out.println(arr);
     }
 
 }
