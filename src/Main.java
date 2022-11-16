03f6ead6a68d88419cc6dbed0ea80cced50a6fd8
KadiyalaSaiSowmith <sai.kadiyala@netcracker.com>
2022-11-16 15:30:04 +0530

    final commit

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,21 @@ public class Main {
     {
         double passingArr[] = new double[10];
         Scanner sc = new Scanner(System.in);
-        // invoking task1
+       /* // invoking task1
         Task_1and2 obj1 = new Task_1and2();
         passingArr=obj1.MaxMinAvg();
         // invoking task2
-        obj1.bubbleSort(passingArr);
-        // invoking task3
+        obj1.bubbleSort(passingArr);*/
+        /*// invoking task3
         System.out.println("enter the start integer");
         int start_int =sc.nextInt();
         System.out.println("enter the end integer");
         int end_int =sc.nextInt();
         Task3 obj3 = new Task3();
-        obj3.primeNumbers(start_int,end_int);
+        obj3.primeNumbers(start_int,end_int);*/
         // invoking task 4
 
-        Task4 obj4 = new Task4();
+        /*ask4 obj4 = new Task4();
         System.out.println("Enter 10 integer elements in an array");
         int[] arr1 = new int[10];
         for(int i=0;i<arr1.length;i++)
@@ -27,22 +27,22 @@ public class Main {
         }
         System.out.println("Enter the element to be removed from the array");
         int removeInt=sc.nextInt();
-        obj4.removalOfTheNumber(removeInt,arr1);
+        obj4.removalOfTheNumber(removeInt,arr1);*/
 
         // invoking task5
-        System.out.println("Enter the String ");
+       /* System.out.println("Enter the String ");
         String str = sc.nextLine();
         Task5 obj5 = new Task5();
-        obj5.countLetters(str);
+        obj5.countLetters(str);*/
         // invoking task6
-        Task6 obj6 = new Task6();
-        obj6.evenNumbersIterator();
+        /*Task6 obj6 = new Task6();
+        obj6.evenNumbersIterator();*/
         // invoking task7
 
-        task7 obj7 = new task7();
+       /* task7 obj7 = new task7();
         System.out.println("Enter the String ");
         String str1= sc.nextLine();
-        obj7.firstUniqueChar(str1);
+        obj7.firstUniqueChar(str1);*/
 
     }
 }
